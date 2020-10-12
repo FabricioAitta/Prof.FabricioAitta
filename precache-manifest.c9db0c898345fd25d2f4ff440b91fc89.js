3d07e324024b1be4fb3b683e8c0fb006a0a91d99
FabricioAitta <alexanderaitta@gmail.com>
2020-10-12 19:15:06 -0300

    Updates

---
+++ precache-manifest.c9db0c898345fd25d2f4ff440b91fc89.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09872fd704cebccebf8fefb9cdce02df",
+    "url": "/Prof.FabricioAitta/index.html"
+  },
+  {
+    "revision": "275f9e7824beb8ccb9d0",
+    "url": "/Prof.FabricioAitta/static/css/main.1cab6db5.chunk.css"
+  },
+  {
+    "revision": "86c2afef2819477fc331",
+    "url": "/Prof.FabricioAitta/static/js/2.de961763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Prof.FabricioAitta/static/js/2.de961763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275f9e7824beb8ccb9d0",
+    "url": "/Prof.FabricioAitta/static/js/main.fd5f4f55.chunk.js"
+  },
+  {
+    "revision": "1766de9804e71c42d7f0",
+    "url": "/Prof.FabricioAitta/static/js/runtime-main.19f77802.js"
+  },
+  {
+    "revision": "d281eb6008257f399597601722f37bbe",
+    "url": "/Prof.FabricioAitta/static/media/body.d281eb60.jpg"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/Prof.FabricioAitta/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "bf89a9a5af4c2b2874073f5357ea5ab8",
+    "url": "/Prof.FabricioAitta/static/media/kb.bf89a9a5.jpg"
+  },
+  {
+    "revision": "e00e0e60a27e72b6b9e65a221c37f3e0",
+    "url": "/Prof.FabricioAitta/static/media/linktree.e00e0e60.png"
+  },
+  {
+    "revision": "d7ea65ef5208c01209fb7199ce80af8f",
+    "url": "/Prof.FabricioAitta/static/media/man.d7ea65ef.jpg"
+  },
+  {
+    "revision": "f2deea51e3072bd46a7e89500910c133",
+    "url": "/Prof.FabricioAitta/static/media/manfuerte.f2deea51.jpg"
+  },
+  {
+    "revision": "eb9090b3e04857a01f2ea01dc4405e34",
+    "url": "/Prof.FabricioAitta/static/media/perfil.eb9090b3.png"
+  },
+  {
+    "revision": "4c89acb6020156515590351fec175212",
+    "url": "/Prof.FabricioAitta/static/media/stretching.4c89acb6.jpg"
+  }
+]);
